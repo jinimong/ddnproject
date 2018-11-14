a7b4597518875d625e39198191451dc6ab28381c
jw.kim <jw.kim@datadriven.kr>
2018-11-14 17:33:58 +0900

    ddn django project commit first

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+
+def post_list(request):
+    qs = Post.objects.all()
+    qs = qs.filter(created_date__lte=timezone.now())
+    qs = qs.order_by('-created_date')
+
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
