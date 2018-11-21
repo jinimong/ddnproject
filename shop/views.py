d24773787776275fd39340f025b4d7719766f689
jwkim <phoongdeng@gmail.com>
2018-11-21 15:19:25 +0900

    add shop item_list view

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Item
 
 def archives_year(request, year):
     return HttpResponse('{}년도..'.format(year))
+
+def item_list(request):
+    qs = Item.objects.all()
+
+    word = request.GET.get('word', '')
+    if word:
+        qs = qs.filter(name__icontains=word)
+
+    return render(request, 'shop/item_list.html', {
+        'item_list': qs,
+        'word': word,
+    })
