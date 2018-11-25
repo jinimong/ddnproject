19505505a80a09757b47bc72b4fb448519fc8ec5
jwkim <jw.kim@datadrivnen.kr>
2018-11-26 00:18:01 +0900

    Modify settings key error

--- lemon/settings/prod_heroku.py
+++ lemon/settings/prod_heroku.py
@@ -1,13 +1,16 @@
 from .common import *
 from .secret import db_info
+from os.path import basename, splitext
+
+filename = splitext(basename(__file__))[0]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': db_info['NAME'],
-        'USER': db_info['USER'],
-        'PASSWORD': db_info['PASSWORD'],
-        'HOST': db_info['HOST'],
-        "PORT": db_info['PORT'],
+        'NAME': db_info[filename]['NAME'],
+        'USER': db_info[filename]['USER'],
+        'PASSWORD': db_info[filename]['PASSWORD'],
+        'HOST': db_info[filename]['HOST'],
+        "PORT": db_info[filename]['PORT'],
     }
 }
