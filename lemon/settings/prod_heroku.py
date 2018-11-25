7171a55d06887bba8c4169bc13bbf6e4f5497dff
Unknown <phoongdeng@gmail.com>
2018-11-26 00:06:40 +0900

    mysql 연동 테스트
    배포버전에서 mysql 로 연결하기 테스트 중

--- lemon/settings/prod_heroku.py
+++ lemon/settings/prod_heroku.py
@@ -1,1 +1,13 @@
 from .common import *
+from .secret import db_info
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': db_info['NAME'],
+        'USER': db_info['USER'],
+        'PASSWORD': db_info['PASSWORD'],
+        'HOST': db_info['HOST'],
+        "PORT": db_info['PORT'],
+    }
+}
