e62f4c577e4af767ac654dd384518b538a628747
Unknown <phoongdeng@gmail.com>
2018-12-10 02:24:17 +0900

    회원가입 기능 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,20 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('settings.LOGIN_URL')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
 
 @login_required
 def profile(request):
