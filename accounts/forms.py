97dd57df68c591e13330381735e5607e662bc2cb
jwkim <phoongdeng@gmail.com>
2018-12-22 14:47:10 +0900

    회원가입 시 이메일 사용가능하도록 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.validators import validate_email
 
 class SignupForm(UserCreationForm):
-    pass
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['username'].validators = [validate_email]
+            self.fields['username'].help_text = 'Enter E-mail Format.'
+            self.fields['username'].label = 'E-mail'
+            
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = user.username
+        if commit:
+            user.save()
+        return user
