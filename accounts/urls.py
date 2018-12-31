371ec3abc8a3294efaa4b632beae8399999f7efd
jwkim <phoongdeng@gmail.com>
2018-12-31 20:21:31 +0900

    이메일 인증으로 로그인 가능하도록 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('login/url/', views.RequestLoginViaUrlView.as_view(), name='request_login_via_url'),
+    path('login/<uidb64>/<token>/', views.login_via_url, name='login_via_url'),
 ]
